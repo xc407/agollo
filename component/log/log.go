1f799da2f8f51c2b08b93d565716f2fd51aed9cd
zhouyu <zhouyu2014@xiaochuankeji.cn>
2020-05-19 20:33:36 +0800

    mod log

--- component/log/log.go
+++ component/log/log.go
@@ -14,61 +14,85 @@ func InitLogger(ILogger LoggerInterface) {
 
 //LoggerInterface 日志接口
 type LoggerInterface interface {
-	Debugf(format string, params ...interface{})
+	//Debugf(format string, params ...interface{})
+	//
+	//Infof(format string, params ...interface{})
+	//
+	//Warnf(format string, params ...interface{}) error
+	//
+	//Errorf(format string, params ...interface{}) error
+	//
+	//Debug(v ...interface{})
+	//
+	//Info(v ...interface{})
+	//
+	//Warn(v ...interface{}) error
+	//
+	//Error(v ...interface{}) error
 
-	Infof(format string, params ...interface{})
+	Debug(arg0 interface{}, args ...interface{})
 
-	Warnf(format string, params ...interface{}) error
+	Info(arg0 interface{}, args ...interface{})
 
-	Errorf(format string, params ...interface{}) error
+	Warn(arg0 interface{}, args ...interface{}) error
 
-	Debug(v ...interface{})
-
-	Info(v ...interface{})
-
-	Warn(v ...interface{}) error
-
-	Error(v ...interface{}) error
+	Error(arg0 interface{}, args ...interface{}) error
 }
 
 //Debugf debug 格式化
 func Debugf(format string, params ...interface{}) {
-	Logger.Debugf(format, params)
+	Logger.Debug(format, params)
 }
 
 //Infof 打印info
 func Infof(format string, params ...interface{}) {
-	Logger.Infof(format, params)
+	Logger.Info(format, params)
 }
 
 //Warnf warn格式化
 func Warnf(format string, params ...interface{}) error {
-	return Logger.Warnf(format, params)
+	return Logger.Warn(format, params)
 }
 
 //Errorf error格式化
 func Errorf(format string, params ...interface{}) error {
-	return Logger.Errorf(format, params)
+	return Logger.Error(format, params)
 }
 
 //Debug 打印debug
 func Debug(v ...interface{}) {
-	Logger.Debug(v)
+	if len(v) > 1 {
+		Logger.Debug(v[0], v[1:]...)
+	} else {
+		Logger.Debug(v)
+	}
 }
 
 //Info 打印Info
 func Info(v ...interface{}) {
-	Logger.Info(v)
+	if len(v) > 1 {
+		Logger.Info(v[0], v[1:]...)
+	} else {
+		Logger.Info(v)
+	}
 }
 
 //Warn 打印Warn
 func Warn(v ...interface{}) {
-	Logger.Warn(v)
+	if len(v) > 1 {
+		Logger.Warn(v[0], v[1:]...)
+	} else {
+		Logger.Warn(v)
+	}
 }
 
 //Error 打印Error
 func Error(v ...interface{}) {
-	Logger.Error(v)
+	if len(v) > 1 {
+		Logger.Error(v[0], v[1:]...)
+	} else {
+		Logger.Error(v)
+	}
 }
 
 //DefaultLogger 默认日志实现
@@ -96,21 +120,21 @@ func (d *DefaultLogger) Errorf(format string, params ...interface{}) error {
 }
 
 //Debug 打印debug
-func (d *DefaultLogger) Debug(v ...interface{}) {
+func (d *DefaultLogger) Debug(arg0 interface{}, args ...interface{}) {
 
 }
 
 //Info 打印Info
-func (d *DefaultLogger) Info(v ...interface{}) {
+func (d *DefaultLogger) Info(arg0 interface{}, args ...interface{}) {
 
 }
 
 //Warn 打印Warn
-func (d *DefaultLogger) Warn(v ...interface{}) error {
+func (d *DefaultLogger) Warn(arg0 interface{}, args ...interface{}) error {
 	return nil
 }
 
 //Error 打印Error
-func (d *DefaultLogger) Error(v ...interface{}) error {
+func (d *DefaultLogger) Error(arg0 interface{}, args ...interface{}) error {
 	return nil
 }
